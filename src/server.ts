3dc1bf017cc05cc9ef10b11e98683d3b4492115f
VidsSkids <victor.massonnat@epitech.eu>
2025-11-10 11:15:49 +0100

    [FIX] - ass default cors configuration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from 'dotenv';
 import connectDB from './config/db';
 import authRoutes from './routes/auth.routes';
@@ -15,6 +16,7 @@ export default app;
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
