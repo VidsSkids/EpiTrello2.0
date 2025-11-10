e1573d5ac56504b5c0d054b4612f971df18b3423
VidsSkids <victor.massonnat@epitech.eu>
2025-11-10 16:10:17 +0100

    [ADD] - permission middleware

--- src/routes/column.routes.ts
+++ src/routes/column.routes.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import ColumnController from '../controllers/column.controller';
 import { validateColumnCreate, validateColumnReorder } from '../validators/column.validator';
 import { authMiddleware } from '../middlewares/auth.middleware';
+import { permissionMiddleware } from '../middlewares/permission.middleware';
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/', authMiddleware, validateColumnCreate, (req, res, next) => ColumnController.create(req, res, next));
-router.patch('/:columnId', authMiddleware, validateColumnCreate, (req, res, next) => ColumnController.update(req, res, next));
-router.patch('/:columnId/reorder', authMiddleware, validateColumnReorder, (req, res, next) => ColumnController.reorder(req, res, next));
-router.delete('/:columnId', authMiddleware, (req, res, next) => ColumnController.delete(req, res, next));
+router.post('/', authMiddleware, validateColumnCreate, permissionMiddleware, (req, res, next) => ColumnController.create(req, res, next));
+router.patch('/:columnId', authMiddleware, validateColumnCreate, permissionMiddleware, (req, res, next) => ColumnController.update(req, res, next));
+router.patch('/:columnId/reorder', authMiddleware, validateColumnReorder, permissionMiddleware, (req, res, next) => ColumnController.reorder(req, res, next));
+router.delete('/:columnId', authMiddleware, permissionMiddleware, (req, res, next) => ColumnController.delete(req, res, next));
 
 export default router;
